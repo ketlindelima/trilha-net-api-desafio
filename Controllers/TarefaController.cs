ad1c5c9b12ca91dfd944f8d211e5365fbd498f08
Ketlin de Lima <ketlindelima@outlook.com>
2024-05-30 18:11:42 -0300

    Get by Id route created

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -18,10 +18,13 @@ namespace TrilhaApiDesafio.Controllers
         [HttpGet("{id}")]
         public IActionResult ObterPorId(int id)
         {
-            // TODO: Buscar o Id no banco utilizando o EF
-            // TODO: Validar o tipo de retorno. Se não encontrar a tarefa, retornar NotFound,
+            //Busca o Id no banco utilizando o EF
+            var tarefa = _context.Tarefas.Find(id);
+            //Valida o tipo de retorno. Se não encontrar a tarefa, retornar NotFound,
+            if (tarefa == null)
+                return NoContent();
             // caso contrário retornar OK com a tarefa encontrada
-            return Ok();
+            return Ok(tarefa);
         }
 
         [HttpGet("ObterTodos")]
